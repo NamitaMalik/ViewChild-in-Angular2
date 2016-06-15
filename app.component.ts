8e4146f7d945431baf1fdd42a13d1f5c9fe59350
Namita Malik <namita1990@gmail.com>
2016-06-15 14:09:02 +0530

    Initial commit

---
+++ app.component.ts
@@ -0,0 +1,17 @@
+/**
+ * Created by namita on 6/12/16.
+ */
+
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+    <div>Hello World
+    </div>
+    `
+})
+
+export class AppComponent {
+
+}
---
+++ main.ts
@@ -0,0 +1,6 @@
+/**
+ * Created by namita on 6/12/16.
+ */
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app.component';
+bootstrap(AppComponent);
