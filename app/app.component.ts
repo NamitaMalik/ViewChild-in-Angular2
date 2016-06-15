fa5792f8afdc2f3393c677d0942c9c8bb833094c
Namita Malik <namita1990@gmail.com>
2016-06-15 16:24:32 +0530

    Making child component

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,13 +3,17 @@
  */
 
 import {Component} from '@angular/core';
+import {ChildComponent} from './child.component';
 
 @Component({
     selector: 'my-app',
     template: `
-    <div>Hello World
+    <div>
+    Hello World
+    <child-component></child-component>
     </div>
-    `
+    `,
+    directives:[ChildComponent]
 })
 
 export class AppComponent {
---
+++ app/child.component.ts
@@ -0,0 +1,18 @@
+/**
+ * Created by namita on 6/15/16.
+ */
+
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'child-component',
+    template: `
+    <div>
+    Hello Child!
+    </div>
+    `
+})
+
+export class ChildComponent {
+
+}
