ee67cc6d68967ea7e5e708c0f8b067273a3f4e7a
AmitThakkar <vigildbest@gmail.com>
2016-06-25 17:23:39 +0530

    Adding code formating.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,26 +1,25 @@
 /**
- * Created by namita on 6/12/16.
+ * Created by Namita Malik on 6/12/16.
  */
-
-import {Component,ViewChild} from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {ChildComponent} from './child.component';
 
 @Component({
     selector: 'my-app',
     template: `
     <div>
-    <h1>Parent Component</h1>
-    <button (click)="showHideText()">Show/Hide Child Component Text</button>
-    <child-component></child-component>
+        <h1>Parent Component</h1>
+        <button (click)="showHideText()">Show/Hide Child Component Text</button>
+        <child-component></child-component>
     </div>
     `,
-    directives:[ChildComponent]
+    directives: [ChildComponent]
 })
 
 export class AppComponent {
     @ViewChild(ChildComponent) childComponent:ChildComponent;
-    showHideText(){
+
+    showHideText() {
         this.childComponent.toggleVisibility('Parent Component');
     }
-
 }
